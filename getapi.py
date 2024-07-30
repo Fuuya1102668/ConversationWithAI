b6bcb09adf456e3b2aaa446f8ef6c252ee7559b5
Fuya IWASHIMA <c1102668@planet.kanazawa-it.ac.jp>
2024-07-30 15:13:37 +0900

    ipアドレスとportを.envから取得できるように変更した．

--- getapi.py
+++ getapi.py
@@ -5,7 +5,26 @@ def get_api():
     load_dotenv()
     return os.environ["API_KEY"]
 
+def get_slave_ip()
+    load_dotenv()
+    return os.environ["SLAVE_IP"]
+
+def get_slave_port()
+    load_dotenv()
+    return os.environ["SLAVE_PORT"]
+
+def get_master_ip()
+    load_dotenv()
+    return os.environ["MASTER_IP"]
+
+def get_master_port()
+    load_dotenv()
+    return os.environ["MASTER_PORT"]
+
 if __name__ == "__main__":
-    api = get_api()
-    print(api)
+    print("api : ", get_api()
+    print("slave ip : ", get_slave_ip())
+    print("slave port : ", get_slave_port())
+    print("master ip : ", get_master_ip())
+    print("master port : ", get_master_port())
 
