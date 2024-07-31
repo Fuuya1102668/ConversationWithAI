d3de76c24b3d31cac7824c25df6238c7f79f58b0
Fuya IWASHIMA <c1102668@planet.kanazawa-it.ac.jp>
2024-07-31 18:54:05 +0900

    UDPの実装

--- master.py
+++ master.py
@@ -1,25 +1,66 @@
+import character_langchain02_RAG_PDF as rag
+import text2speech as t2s
+import getapi as get
+import pickle
+import os
+
+import io
+import simpleaudio as sa
+
 import socket
 
-##########
-# MASTER #
-##########
+os.environ["OPENAI_API_KEY"] = get.get_api()
+master_ip = get.get_maste_ip()
+slave_ip = get.get_slave_ip()
+slave_port = get.get_slave_port()
 
-slave_ip = "192.168.11.48"
-slave_port = 12346
-
-master_port = 12345
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF.INET, socket.SOCK_DGRAM)
 s.bind(("", master_port))
 
-print("Conectino")
+config = {"configurable": {"session_id": "zunda"}}
+model_name = "ft:gpt-3.5-turbo-0125:personal::9ol99gYa"
+directory = "./rag_source"
+file = "*.pdf"
+# ここは触らない
+contextualize_q_system_prompt = (
+    "チャット履歴と，チャット履歴のコンテキストを参照する可能性のある最新のユーザの質問が与えられた場合，チャット履歴を優先的に参照して質問に答えてください．"
+)
+# ここにBotのプロンプトを書く．
+qa_system_prompt = "あなたの名前はずんだもんです．これからはすべて日本語で回答してください．また，回答は必ず，contextとhistoryを参照してから行ってください．{context}"
+
+# テキストモデルの呼び出し
+text_model = rag.create_chat_model(model_name)
+# pdfファイルの読み込み
+retriever = rag.loade_pdf(directory, file)
+# chainの作成
+conversational_rag_chain = rag.create_chain(text_model, retriever, contextualize_q_system_prompt, qa_system_prompt)
+
+try:
+    with open("history_" + config["configurable"]["session_id"] + ".plk", "rb") as f:
+        chat_history = pickle.load(f)
+except FileNotFoundError:
+    chat_history = []
 
 while True:
-    data, addr = s.recvfrom(65535)
-    print(data.decode())
+    outputs = ""
+    inputs, addr = s.recvfrom(65535)
+    if inputs.lower() == "exit":
+        break
+    outputs = conversational_rag_chain.invoke(
+        {"input":inputs},
+        config=config,
+        )
+    chat_history = rag.add_history(chat_history, inputs, outputs)
+    response = t2s.generate_speech(outputs["answer"]
+    s.sendto(picle.dumps(response), (slave_ip, slave_port))
+    #print("ずんだもん：", outputs["answer"])
+    #sa.WaveObject.from_wave_file(io.BytesIO(response.content)).play()
+    #audio_segment = AudioSegment.from_file(io.BytesIO(response.content),foemat="wav")
+    #play(audio_segment)
+    #print(chat_history)
 
-    a = input("入力：")
-    s.sendto("gogogo".encode(), (slave_ip, slave_port))
-
+with open("history_" + config["configurable"]["session_id"] + ".plk", "wb") as f:
+    pickle.dump(chat_history, f)
+print("ずんだもん：会話内容を保存したのだ．また今度なのだ．")
 s.close()
 
--- slave.py
+++ slave.py
@@ -1,26 +1,31 @@
+import getapi as get
 import socket
+
+import io
+import simpleaudio as sa
 
 #########
 # SLAVE #
 #########
 
-master_ip = "192.168.11.50"
-master_port = 12345
-
-slave_port = 12346
+master_ip = get.get_master_ip()
+master_port = get.get_master_port()
+slave_port = get.slava_port()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", slave_port))
 
-print("Conection")
+print("Conection server")
 
 while True:
     inputs = input("  あなた  ：")
+    s.sendto(inputs.encode(), (master_ip, master_port))
     if inputs.lower() == "exit":
         break
-    s.sendto(inputs.encode(), (master_ip, master_port))
 
-    data, addr = s.recvfrom(1024)
+    response, addr = s.recvfrom(65535)
+    response = pickle.load(response)
+    sa.WaveObject.from_wave_file(io.BytesIO(response.content)).play()
     print(date.decode())
 
 s.close()
