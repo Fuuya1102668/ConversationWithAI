4792c85b1f8b4da8c0deefa0e6a6b29aab51031f
Fuya IWASHIMA <c1102668@planet.kanazawa-it.ac.jp>
2024-08-02 15:20:18 +0900

    TCPに対応

--- slave_TCP.py
+++ slave_TCP.py
@@ -17,19 +17,33 @@ slave_port = int(get.get_slave_port())
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((master_ip, master_port))
 
-print("Conection server")
+print("Connected to server")
 
-while True:
-    inputs = input("  あなた  ：")
-    s.sendall(inputs.encode())
-    if inputs.lower() == "exit":
-        break
-    response = s.recv(1000000)
-    print("data receved")
-    print("Total : " + str(len(response)))
-    response = pickle.loads(response)
-    sa.WaveObject.from_wave_file(io.BytesIO(response.content)).play()
-    print(date.decode())
+try:
+    while True:
+        inputs = input("  あなた  ：")
+        s.sendall(inputs.encode())
+        if inputs.lower() == "exit":
+            break
+        
+        response = b''
+        while True:
+            part = s.recv(4096)
+            if not part:
+                break
+            response += part
+        
+        print("Data received")
+        print("Total : " + str(len(response)))
+        
+        response_content = pickle.loads(response)
+        sa.WaveObject.from_wave_file(io.BytesIO(response_content)).play()
+        print("Response played")
 
-s.close()
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    s.close()
+    print("Connection closed")
 
