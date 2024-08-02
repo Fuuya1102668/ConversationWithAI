e947342a6c55111e37b88a8806a15799c3c3592c
Fuya IWASHIMA <c1102668@planet.kanazawa-it.ac.jp>
2024-08-02 11:25:32 +0900

    TCPに対応

---
+++ slave_TCP.py
@@ -0,0 +1,33 @@
+import getapi as get
+import socket
+
+import io
+import simpleaudio as sa
+
+#########
+# SLAVE #
+#  TCP  #
+#########
+
+master_ip = get.get_master_ip()
+master_port = get.get_master_port()
+slave_port = get.slava_port()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((master_ip, master_port))
+
+print("Conection server")
+
+while True:
+    inputs = input("  あなた  ：")
+    s.sendall(inputs.encode(), (master_ip, master_port))
+    if inputs.lower() == "exit":
+        break
+    response = s.recv(1000000)
+    print("Total : " + str(len(responde)))
+    response = pickle.loads(response)
+    sa.WaveObject.from_wave_file(io.BytesIO(response.content)).play()
+    print(date.decode())
+
+s.close()
+
